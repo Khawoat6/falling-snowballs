4389c902e0a0d1ea545c884377ddacbeaeb22574
oat phattaraphon <phattaraphon.c@ku.th>
2021-01-07 14:34:45 +0700

    initial js21days 01 - falling-snow

---
+++ script.js
@@ -0,0 +1,85 @@
+(() => {
+    function setup() {
+        const canvas = document.getElementById('falling-snow-canvas');
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        return {
+            canvas,
+            canvasContext: canvas.getContext('2d'),
+            numberOfSnowBalls: 250
+        }
+    }
+    function random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+
+    }
+
+    function createSnowBalls(canvas, numberOfSnowBalls) {
+        return [...Array(numberOfSnowBalls)].map(() => {
+            return {
+                x: random(0, canvas.width),
+                y: random(0, canvas.height),
+                radius: random(2, 4),
+                opacity: random(0.5, 1),
+                speedX: random(-5, 5),
+                speedY: random(1, 3)
+            }
+        });
+    }
+
+    function drawSnowBall(canvasContext, snowBall) {
+        canvasContext.beginPath();
+        canvasContext.arc(snowBall.x, snowBall.y, snowBall.radius, 0, Math.PI *2);
+        canvasContext.fillStyle = `rgba(255, 255, 255, ${snowBall.opacity})`;
+        canvasContext.fill();
+    }
+    function moveSnowBall(canvas, snowBall) {
+        snowBall.x += snowBall.speedX;
+        snowBall.y += snowBall.speedY;
+
+        if (snowBall.x > canvas.width) {
+            snowBall.s = 0;
+        } else if (snowBall.x < 0) {
+            snowBall.x = canvas.width;
+        }
+
+        if (snowBall.y > canvas.height) {
+            snowBall.y = 0;
+        }
+    }
+
+
+    function run() {
+        const {canvas, canvasContext, numberOfSnowBalls} = setup();
+        const snowBalls = createSnowBalls(canvas, numberOfSnowBalls);
+
+        setInterval(() => {
+            canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+            snowBalls.forEach((snowBall) => drawSnowBall(canvasContext, snowBall));
+            snowBalls.forEach((snowBall) => moveSnowBall(canvas, snowBall));
+        }, 50)
+    }
+    run();
+
+})();
+
+
+/* สิ่งที่ได้เรียนรู้จาก EP.1
+1. Selector element ด้วย js (getElementById)
+2. ขนาดของ Window (window.innerWidth, window.innerHeight)
+3. การเรียกใช้ Context ของ Canvas (canvas.getContext('2d'))
+4. วิธีวาดวงกลมบน Canvas
+	- .beginPath();
+	- .arc();
+	- .fillStyle
+	- .fill()
+5. ฟังก์ชั่นต่างๆของ JS ในรูปแบบ ES6
+	- Arrow function (() => {})
+	- Spread Operator (...)
+	- .map()
+	- .forEach()
+	- Object Destructuring เช่น const { canvas } = setup();
+6. ฟังก์ชัน Math ของ JS
+	- Math.floor (ปัดเลขให้เป็นจำนวนเต็ม)
+	- Math.random (สุ่มเลขระหว่าง 0, 1) */
